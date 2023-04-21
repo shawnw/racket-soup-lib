d35f0dd71860ca21d23bce520d864ebd8240d8cb
Shawn Wagner <shawnw.mobile@gmail.com>
2023-04-21 05:29:47 -0700

    soup-lib/parameter: Add parameterize*

--- parameter.rkt
+++ parameter.rkt
@@ -4,7 +4,7 @@
          (for-syntax racket/base syntax/parse))
 (module+ test (require rackunit))
 
-(provide define-parameter define-boolean-parameter)
+(provide define-parameter define-boolean-parameter parameterize*)
 
 (define-syntax (define-parameter stx)
   (syntax-parse stx
@@ -24,6 +24,14 @@
     ((define-boolean-parameter name:id initial-value:boolean obj-name:id)
      #'(define-parameter name initial-value make-boolean obj-name))))
 
+(define-syntax (parameterize* stx)
+  (syntax-parse stx
+    ((_ () body:expr ...)
+     #'(let () body ...))
+    ((_ ((param:id val:expr) . rest-params) body:expr ...)
+     #'(parameterize ((param val))
+         (parameterize* rest-params body ...)))))
+
 (module+ test
   (define-boolean-parameter foo)
   (check-equal? (foo) #t)
@@ -31,4 +39,13 @@
     (check-equal? (foo) #t))
   (parameterize ([foo #f])
     (check-equal? (foo) #f))
-  (check-equal? (object-name foo) 'foo))
+  (check-equal? (object-name foo) 'foo)
+
+
+  (define-parameter a 2)
+  (define-parameter b #f)
+  (check-equal?
+   (parameterize* ([a 4]
+                   [b (* (a) 2)])
+     (b))
+   8))
