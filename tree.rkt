58ddb108247ad65a437510cef9275213fd9093b8
Shawn Wagner <shawnw.mobile@gmail.com>
2023-04-30 02:38:31 -0700

    soup-lib/tree: Only install continuation prompts when requested

--- tree.rkt
+++ tree.rkt
@@ -10,21 +10,21 @@
 (provide
  copy-tree tree-equal? subst subst-if ; from soup-lib/list
  (contract-out
-  [map-tree (->* ((-> any/c any/c) any/c) (#:tag (prompt-tag/c any/c) #:traversal traverse-type/c) any/c)]
-  [walk-tree (->* ((-> any/c any) any/c) (#:tag (prompt-tag/c any/c) #:traversal traverse-type/c) void?)]
+  [map-tree (->* ((-> any/c any/c) any/c) (#:tag (or/c (prompt-tag/c any/c) #f) #:traversal traverse-type/c) any/c)]
+  [walk-tree (->* ((-> any/c any) any/c) (#:tag (or/c (prompt-tag/c any/c) #f) #:traversal traverse-type/c) void?)]
   [leaf-map (-> (-> any/c any/c) any/c any/c)]
   [leaf-walk (-> (-> any/c any) any/c void?)]
   [occurs-if (->* ((-> any/c any/c) any/c) (#:traversal traverse-type/c #:key (-> any/c any/c)) (values any/c boolean?))]
   [occurs (->* (any/c any/c) (#:traversal traverse-type/c #:key (-> any/c any/c) #:test (-> any/c any/c any/c)) (values any/c boolean?))]
   ))
 
-(define (map-tree fun tree #:tag [tag (default-continuation-prompt-tag)] #:traversal [traverse 'preorder])
+(define (map-tree fun tree #:tag [tag #f] #:traversal [traverse 'preorder])
   (case traverse
     ((preorder) (map-tree/preorder fun tree tag))
     ((postorder) (map-tree/postorder fun tree tag))
     ((inorder) (map-tree/inorder fun tree tag))))
 
-(define (walk-tree fun tree #:tag [tag (default-continuation-prompt-tag)] #:traversal [traverse 'preorder])
+(define (walk-tree fun tree #:tag [tag #f] #:traversal [traverse 'preorder])
   (void (map-tree (lambda (subtree)
                     (fun subtree)
                     subtree)
@@ -33,39 +33,71 @@
                   #:traversal traverse)))
 
 (define (map-tree/preorder fun tree prompt)
-  (call-with-continuation-prompt
-   (thunk
+  (define (mt/tag tree)
+    (call-with-continuation-prompt
+     (thunk
+      (let ([tree2 (fun tree)])
+        (if (pair? tree2)
+            (reuse-cons (mt/tag (unsafe-car tree2))
+                        (mt/tag (unsafe-cdr tree2))
+                        tree2)
+            tree2)))
+     prompt
+     identity))
+  (define (mt tree)
     (let ([tree2 (fun tree)])
       (if (pair? tree2)
-          (reuse-cons (map-tree/preorder fun (unsafe-car tree2) prompt)
-                      (map-tree/preorder fun (unsafe-cdr tree2) prompt)
+          (reuse-cons (mt (unsafe-car tree2))
+                      (mt (unsafe-cdr tree2))
                       tree2)
           tree2)))
-   prompt
-   identity))
+  (if prompt
+      (mt/tag tree)
+      (mt tree)))
 
 (define (map-tree/postorder fun tree prompt)
+  (define (mt/tag tree)
     (call-with-continuation-prompt
      (thunk
       (if (pair? tree)
-          (let* ([left (map-tree/postorder fun (unsafe-car tree) prompt)]
-                 [right (map-tree/postorder fun (unsafe-cdr tree) prompt)]
+          (let* ([left (mt/tag (unsafe-car tree))]
+                 [right (mt/tag (unsafe-cdr tree))]
                  [tree2 (reuse-cons left right tree)])
             (fun tree2))
           (fun tree)))
      prompt
      identity))
+  (define (mt tree)
+    (if (pair? tree)
+        (let* ([left (mt (unsafe-car tree))]
+               [right (mt (unsafe-cdr tree))]
+               [tree2 (reuse-cons left right tree)])
+          (fun tree2))
+        (fun tree)))
+  (if prompt
+      (mt/tag tree)
+      (mt tree)))
 
 (define (map-tree/inorder fun tree prompt)
-  (call-with-continuation-prompt
-   (thunk
+  (define (mt/tag tree)
+    (call-with-continuation-prompt
+     (thunk
+      (if (pair? tree)
+          (let* ([left (mt/tag (unsafe-car tree))]
+                 [tree2 (fun (reuse-cons left (unsafe-cdr tree) tree))])
+            (reuse-cons (car tree2) (mt/tag (cdr tree2)) tree2))
+          (fun tree)))
+     prompt
+     identity))
+  (define (mt tree)
     (if (pair? tree)
-        (let* ([left (map-tree/inorder fun (unsafe-car tree) prompt)]
+        (let* ([left (mt (unsafe-car tree))]
                [tree2 (fun (reuse-cons left (unsafe-cdr tree) tree))])
-          (reuse-cons (car tree2) (map-tree/inorder fun (cdr tree2) prompt) tree2))
+          (reuse-cons (car tree2) (mt (cdr tree2)) tree2))
         (fun tree)))
-   prompt
-   identity))
+  (if prompt
+      (mt/tag tree)
+      (mt tree)))
 
 (define (leaf-walk fun tree)
   (cond
@@ -112,13 +144,15 @@
                (leaf-map (compose round sqrt) '(((4 1) 25) (9 100) 64))
                '(((2 1) 5) (3 10) 8))
 
+  (define skipper (make-continuation-prompt-tag))
   (test-equal? "map-tree"
                (map-tree (lambda (subtree)
                            (if (and (pair? subtree)
                                     (eq? (car subtree) 'skip-me))
-                               (abort/cc (default-continuation-prompt-tag) 'skipped)
+                               (abort/cc skipper 'skipped)
                                subtree))
-                         '((a (b) (c (skip-me d (e f))))))
+                         '((a (b) (c (skip-me d (e f)))))
+                         #:tag skipper)
                '((a (b) (c skipped))))
 
 
