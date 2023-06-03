674fc8450c34eb92a95173409e63bc7fcc539711
Shawn Wagner <shawnw.mobile@gmail.com>
2023-06-03 03:19:21 -0700

    Add typed-racket-lib dependency

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "soup-lib")
-(define deps '("base" "srfi-lite-lib" "extra-srfi-libs"))
+(define deps '("base" "typed-racket-lib" "srfi-lite-lib" "extra-srfi-libs"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/soup-lib.scrbl" ())))
 (define pkg-desc "Assorted useful routines")
