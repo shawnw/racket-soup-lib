b8791c12b6a733503309e6f205afe7f7da88a8f2
Shawn Wagner <shawnw.mobile@gmail.com>
2024-09-27 23:14:13 -0700

    Add missing dependency to info.rkt

--- info.rkt
+++ info.rkt
@@ -1,10 +1,10 @@
 #lang info
 (define collection "soup-lib")
-(define deps '("base" "typed-racket-lib" "srfi-lite-lib" "extra-srfi-libs"
-                      ("racket" #:version "8.11")))
+(define deps '("base" "typed-racket-lib" "srfi-lite-lib" "extra-srfi-libs" "syntax-classes-lib"
+                      ("racket" #:version "8.12")))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/soup-lib.scrbl" ())))
 (define pkg-desc "Assorted useful routines")
-(define version "1.0")
+(define version "1.1")
 (define pkg-authors '(shawnw))
 (define license '(Apache-2.0 OR MIT))
