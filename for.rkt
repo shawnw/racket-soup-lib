f64f8b6d0410b84b12d7ddee0ecc28efc0416df2
Shawn Wagner <shawnw.mobile@gmail.com>
2025-04-06 12:40:44 -0700

    Avoid evaluting the length expression more than once in for/string, for/bytes, etc.

--- for.rkt
+++ for.rkt
@@ -20,128 +20,128 @@
                                   [_ (source)
                                      sequence?
                                      #;(if (string? source)
-                                         (sequence/c (cons/c string? (listof (or/c string? #f))))
-                                         (sequence/c (cons/c bytes? (listof (or/c bytes? f)))))])]
+                                           (sequence/c (cons/c string? (listof (or/c string? #f))))
+                                           (sequence/c (cons/c bytes? (listof (or/c bytes? f)))))])]
           [in-char-range (-> char? char? sequence? #;(sequence/c char?))]
           [in-conses (-> list? sequence? #;(sequence/c list?))]
           [in-list-sequence (-> sequence? sequence?)]))
 
-(define-syntax-parse-rule (for/string (~optional (~seq #:length slen:expr)) clauses body ... tail-expr)
+(define-syntax-parse-rule (for/string (~optional (~seq #:length slen:expr) #:defaults ([slen #'32])) clauses body ... tail-expr)
   #:with original this-syntax
   #:with ((pre-body ...) (post-body ...)) (split-for-body this-syntax #'(body ... tail-expr))
-  #:with rlen #'(~? slen 32)
-  (for/fold/derived original ([chars (make-string rlen)]
-                              [len rlen]
-                              [n 0]
-                              #:result (if (unsafe-fx= n len) chars (substring chars 0 n)))
-    clauses
-    pre-body ...
-    (let ([ch (let () post-body ...)])
-      (cond
-        ((char? ch)
-         (cond
-           ((unsafe-fx< n len)
-            (unsafe-string-set! chars n ch)
-            (values chars len (unsafe-fx+ n 1)))
-           (else
-            (let ([new-chars (make-string (fl->fx (* len 1.5)))])
-              (string-copy! new-chars 0 chars)
-              (unsafe-string-set! new-chars n ch)
-              (values new-chars (unsafe-string-length new-chars) (unsafe-fx+ n 1))))))
-        ((string? ch)
-         (let ([xlen (unsafe-string-length ch)])
+  (let ([rlen slen])
+    (for/fold/derived original ([chars (make-string rlen)]
+                                [len rlen]
+                                [n 0]
+                                #:result (if (unsafe-fx= n len) chars (substring chars 0 n)))
+      clauses
+      pre-body ...
+      (let ([ch (let () post-body ...)])
+        (cond
+          ((char? ch)
            (cond
-             ((unsafe-fx< (unsafe-fx+ n xlen) len)
-              (string-copy! chars n ch)
-              (values chars len (unsafe-fx+ n xlen)))
+             ((unsafe-fx< n len)
+              (unsafe-string-set! chars n ch)
+              (values chars len (unsafe-fx+ n 1)))
              (else
-              (let ([new-chars (make-string (unsafe-fx+ (fl->fx (* len 1.5)) xlen))])
+              (let ([new-chars (make-string (fl->fx (* len 1.5)))])
                 (string-copy! new-chars 0 chars)
-                (string-copy! new-chars n ch)
-                (values new-chars (unsafe-string-length new-chars) (unsafe-fx+ n xlen)))))))
-        (else
-         (raise-arguments-error 'for/string "body must return a string or character" "value" ch))))))
+                (unsafe-string-set! new-chars n ch)
+                (values new-chars (unsafe-string-length new-chars) (unsafe-fx+ n 1))))))
+          ((string? ch)
+           (let ([xlen (unsafe-string-length ch)])
+             (cond
+               ((unsafe-fx< (unsafe-fx+ n xlen) len)
+                (string-copy! chars n ch)
+                (values chars len (unsafe-fx+ n xlen)))
+               (else
+                (let ([new-chars (make-string (unsafe-fx+ (fl->fx (* len 1.5)) xlen))])
+                  (string-copy! new-chars 0 chars)
+                  (string-copy! new-chars n ch)
+                  (values new-chars (unsafe-string-length new-chars) (unsafe-fx+ n xlen)))))))
+          (else
+           (raise-arguments-error 'for/string "body must return a string or character" "value" ch)))))))
 
 
-(define-syntax-parse-rule (for*/string (~optional (~seq #:length slen:expr)) clauses body ... tail-expr)
+(define-syntax-parse-rule (for*/string (~optional (~seq #:length slen:expr) #:defaults ([slen #'32])) clauses body ... tail-expr)
   #:with original this-syntax
   #:with ((pre-body ...) (post-body ...)) (split-for-body this-syntax #'(body ... tail-expr))
-  #:with rlen #'(~? slen 32)
-  (for*/fold/derived original ([chars (make-string rlen)]
-                               [len rlen]
-                               [n 0]
-                               #:result (if (unsafe-fx= n len) chars (substring chars 0 n)))
-    clauses
-    pre-body ...
-    (let ([ch (let () post-body ...)])
-      (cond
-        ((char? ch)
-         (cond
-           ((unsafe-fx< n len)
-            (unsafe-string-set! chars n ch)
-            (values chars len (unsafe-fx+ n 1)))
-           (else
-            (let ([new-chars (make-string (fl->fx (* len 1.5)))])
-              (string-copy! new-chars 0 chars)
-              (unsafe-string-set! new-chars n ch)
-              (values new-chars (unsafe-string-length new-chars) (unsafe-fx+ n 1))))))
-        ((string? ch)
-         (let ([xlen (unsafe-string-length ch)])
+  (let ([rlen slen])
+    (for*/fold/derived original ([chars (make-string rlen)]
+                                 [len rlen]
+                                 [n 0]
+                                 #:result (if (unsafe-fx= n len) chars (substring chars 0 n)))
+      clauses
+      pre-body ...
+      (let ([ch (let () post-body ...)])
+        (cond
+          ((char? ch)
            (cond
-             ((unsafe-fx< (unsafe-fx+ n xlen) len)
-              (string-copy! chars n ch)
-              (values chars len (unsafe-fx+ n xlen)))
+             ((unsafe-fx< n len)
+              (unsafe-string-set! chars n ch)
+              (values chars len (unsafe-fx+ n 1)))
              (else
-              (let ([new-chars (make-string (unsafe-fx+ (fl->fx (* len 1.5)) xlen))])
+              (let ([new-chars (make-string (fl->fx (* len 1.5)))])
                 (string-copy! new-chars 0 chars)
-                (string-copy! new-chars n ch)
-                (values new-chars (unsafe-string-length new-chars) (unsafe-fx+ n xlen)))))))
-        (else
-         (raise-arguments-error 'for*/string "body must return a string or character" "value" ch))))))
+                (unsafe-string-set! new-chars n ch)
+                (values new-chars (unsafe-string-length new-chars) (unsafe-fx+ n 1))))))
+          ((string? ch)
+           (let ([xlen (unsafe-string-length ch)])
+             (cond
+               ((unsafe-fx< (unsafe-fx+ n xlen) len)
+                (string-copy! chars n ch)
+                (values chars len (unsafe-fx+ n xlen)))
+               (else
+                (let ([new-chars (make-string (unsafe-fx+ (fl->fx (* len 1.5)) xlen))])
+                  (string-copy! new-chars 0 chars)
+                  (string-copy! new-chars n ch)
+                  (values new-chars (unsafe-string-length new-chars) (unsafe-fx+ n xlen)))))))
+          (else
+           (raise-arguments-error 'for*/string "body must return a string or character" "value" ch)))))))
 
-(define-syntax-parse-rule (for/bytes (~optional (~seq #:length slen:expr)) clauses body ... tail-expr)
+(define-syntax-parse-rule (for/bytes (~optional (~seq #:length slen:expr) #:defaults ([slen #'32])) clauses body ... tail-expr)
   #:with original this-syntax
   #:with ((pre-body ...) (post-body ...)) (split-for-body this-syntax #'(body ... tail-expr))
-  #:with rlen #'(~? slen 32)
-  (for/fold/derived original ([bytes (make-bytes rlen)]
-                              [len rlen]
-                              [n 0]
-                              #:result (if (unsafe-fx= n len) bytes (subbytes bytes 0 n)))
-    clauses
-    pre-body ...
-    (let ([ch (let () post-body ...)])
-      (unless (byte? ch)
-        (raise-arguments-error 'for/bytes "body must return a byte" "value" ch))
-      (if (unsafe-fx< n len)
-          (begin
-            (unsafe-bytes-set! bytes n ch)
-            (values bytes len (unsafe-fx+ n 1)))
-          (let ([new-bytes (make-bytes (fl->fx (* len 1.5)))])
-            (bytes-copy! new-bytes 0 bytes)
-            (unsafe-bytes-set! new-bytes n ch)
-            (values new-bytes (unsafe-bytes-length new-bytes) (unsafe-fx+ n 1)))))))
+  (let ([rlen slen])
+    (for/fold/derived original ([bytes (make-bytes rlen)]
+                                [len rlen]
+                                [n 0]
+                                #:result (if (unsafe-fx= n len) bytes (subbytes bytes 0 n)))
+      clauses
+      pre-body ...
+      (let ([ch (let () post-body ...)])
+        (unless (byte? ch)
+          (raise-arguments-error 'for/bytes "body must return a byte" "value" ch))
+        (if (unsafe-fx< n len)
+            (begin
+              (unsafe-bytes-set! bytes n ch)
+              (values bytes len (unsafe-fx+ n 1)))
+            (let ([new-bytes (make-bytes (fl->fx (* len 1.5)))])
+              (bytes-copy! new-bytes 0 bytes)
+              (unsafe-bytes-set! new-bytes n ch)
+              (values new-bytes (unsafe-bytes-length new-bytes) (unsafe-fx+ n 1))))))))
 
-(define-syntax-parse-rule (for*/bytes (~optional (~seq #:length slen:expr)) clauses body ... tail-expr)
+(define-syntax-parse-rule (for*/bytes (~optional (~seq #:length slen:expr) #:defaults ([slen #'32])) clauses body ... tail-expr)
   #:with original this-syntax
   #:with ((pre-body ...) (post-body ...)) (split-for-body this-syntax #'(body ... tail-expr))
-  #:with rlen #'(~? slen 32)
-  (for*/fold/derived original ([bytes (make-bytes rlen)]
-                               [len rlen]
-                               [n 0]
-                               #:result (if (unsafe-fx= n len) bytes (subbytes bytes 0 n)))
-    clauses
-    pre-body ...
-    (let ([ch (let () post-body ...)])
-      (unless (byte? ch)
-        (raise-arguments-error 'for*/bytes "body must return a byte" "value" ch))
-      (if (unsafe-fx< n len)
-          (begin
-            (unsafe-bytes-set! bytes n ch)
-            (values bytes len (unsafe-fx+ n 1)))
-          (let ([new-bytes (make-bytes (fl->fx (* len 1.5)))])
-            (bytes-copy! new-bytes 0 butes)
-            (unsafe-bytes-set! new-bytes n ch)
-            (values new-bytes (unsafe-bytes-length new-chars) (unsafe-fx+ n 1)))))))
+  (let ([rlen slen])
+    (for*/fold/derived original ([bytes (make-bytes rlen)]
+                                 [len rlen]
+                                 [n 0]
+                                 #:result (if (unsafe-fx= n len) bytes (subbytes bytes 0 n)))
+      clauses
+      pre-body ...
+      (let ([ch (let () post-body ...)])
+        (unless (byte? ch)
+          (raise-arguments-error 'for*/bytes "body must return a byte" "value" ch))
+        (if (unsafe-fx< n len)
+            (begin
+              (unsafe-bytes-set! bytes n ch)
+              (values bytes len (unsafe-fx+ n 1)))
+            (let ([new-bytes (make-bytes (fl->fx (* len 1.5)))])
+              (bytes-copy! new-bytes 0 butes)
+              (unsafe-bytes-set! new-bytes n ch)
+              (values new-bytes (unsafe-bytes-length new-chars) (unsafe-fx+ n 1))))))))
 
 (define-syntax-parse-rule (for/max clauses body ... tail-expr)
   #:with original this-syntax
